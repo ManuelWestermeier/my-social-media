6e5aa3811bd7679e2ec5d15670599f090bb82659
ManuelWestermeier <westermeier111@gmail.com>
2024-05-21 09:23:14 +0200

    first commit

--- src/pages/auth/create-account.jsx
+++ src/pages/auth/create-account.jsx
@@ -1,11 +1,12 @@
 import React, { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-async function createUser({ userName, password }) {
+async function createUser({ userName, password, email }) {
   try {
     const url = new URL(apiUrl + "/create-user");
     url.searchParams.set("user", userName);
     url.searchParams.set("password", password);
+    url.searchParams.set("email", email);
 
     const res = await fetch(url);
     if (!res.ok) {
@@ -33,14 +34,16 @@ function CreateAccount({ setAuth }) {
 
   const userNameInput = useRef();
   const passwordInput = useRef();
+  const emailInput = useRef();
 
   const onSubmit = async (e) => {
     e.preventDefault();
 
     const userName = userNameInput.current?.value;
     const password = passwordInput.current?.value;
+    const email = emailInput.current?.value;
 
-    const result = await createUser({ userName, password });
+    const result = await createUser({ userName, password, email });
 
     if (result[0]) {
       setError("User Created");
@@ -58,8 +61,24 @@ function CreateAccount({ setAuth }) {
   return (
     <form className="auth-form" onSubmit={onSubmit}>
       <h2>Create Account</h2>
-      <input type="text" ref={userNameInput} placeholder="Username..." />
-      <input type="text" ref={passwordInput} placeholder="Password..." />
+      <input
+        name="username"
+        type="text"
+        ref={userNameInput}
+        placeholder="Username..."
+      />
+      <input
+        name="email"
+        type="email"
+        ref={emailInput}
+        placeholder="Email..."
+      />
+      <input
+        name="password"
+        type="password"
+        ref={passwordInput}
+        placeholder="Password..."
+      />
       <p style={{ color: "red" }}>{error}</p>
       <button>Create Account</button>
     </form>
