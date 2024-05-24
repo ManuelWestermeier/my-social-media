88439006bf72ce5735bf502165b35900c2bf0401
ManuelWestermeier <westermeier111@gmail.com>
2024-05-25 00:07:12 +0200

    update

--- src/comp/vide-manager/index.jsx
+++ src/comp/vide-manager/index.jsx
@@ -1,6 +1,7 @@
 import useLocalStorage from "use-local-storage";
 import UserProfileVideoList from "../user-profile-video-list";
 import "./index.css";
+import { Link } from "react-router-dom";
 
 function VideManager({ auth, videos = [], deleteVideoWithId }) {
   const [showViedos, setShowViedos] = useLocalStorage("show-videos", false);
@@ -10,6 +11,7 @@ function VideManager({ auth, videos = [], deleteVideoWithId }) {
       <button onClick={(e) => setShowViedos((o) => !o)}>
         {showViedos ? "Hide" : "Show"} Videos
       </button>
+      {videos.length == 0 && <p>No videos yet <Link to="/upload">[upload video]</Link></p>}
       {showViedos && (
         <UserProfileVideoList
           deleteVideoWithId={deleteVideoWithId}
