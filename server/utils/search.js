a74fcb670c5b80971a29c4283f49f3624315547f
ManuelWestermeier <westermeier111@gmail.com>
2024-05-24 15:37:03 +0200

    update

--- server/utils/search.js
+++ server/utils/search.js
@@ -14,9 +14,8 @@ export default function search(req, res) {
     }
 
     const results = []
-
-    if (req.query.search == "") {
-        return videoSearchItems.slice(0, 5)
+    if (req.query.search.replace(/ /g, "") == "") {
+        return res.json(videoSearchItems.slice(0, 5))
     }
 
     const searchWords = [...new Set(req.query.search.toLowerCase().split(" "))]
